54ca5f16f6a0ab8b4d47cb8227d082996b8479e8
Alterlapsus <israel.delen96@gmail.com>
2025-10-26 14:24:49 -0600

    Configuración de MySQL con Aiven

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-  HOST: process.env.DB_HOST || "localhost",
-  USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "isruzadelensusano",
-  DB: process.env.DB_NAME || "testdb",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
